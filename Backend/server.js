74b8d132670882e827e0b112e5335cf42f376e2a
Pawan <pawanukey13@gmail.com>
2026-09-09 00:42:26 +0530

    Fix backend deployment configuration

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,16 @@
-const app = require("./src/app")
-const dns = require("dns")
-dns.setServers(['8.8.8.8','8.8.4.4'])
-const connectDB = require("./src/databse/db")
-const cors = require('cors')
 require("dotenv").config();
-connectDB()
+
+const app = require("./src/app");
+const dns = require("dns");
+const connectDB = require("./src/databse/db");
+const cors = require("cors");
+
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+connectDB();
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`);
-})
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`server running on port ${PORT}`);
+});
